96b4e6a19409f7a40d64f0a112024bd7b38549d9
Damian <damianmorganti16@hotmail.com>
2025-06-27 16:13:15 -0400

    added FlowBoard

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,7 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import FlowBoard from "./components/FlowBoard/FlowBoard.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <q-btn label="¡Hola!" class="my-btn" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <FlowBoard />
 </template>
-
-<style lang="sass" scoped>
-header
-  line-height: 1.5
-
-.logo
-  display: block
-  margin: 0 auto 2rem
-
-.my-btn
-  background-color: $primary
-  color: $white
-  padding: 1rem
-  border-radius: $border-radius
-
-@media (min-width: 1024px)
-  header
-    display: flex
-    place-items: center
-    padding-right: calc(var(--section-gap) / 2)
-
-  .logo
-    margin: 0 2rem 0 0
-
-  header .wrapper
-    display: flex
-    place-items: flex-start
-    flex-wrap: wrap
-</style>
---
+++ src/components/FlowBoard/FlowBoard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flow-board q-pa-md" style="height: 100vh; width: 100vw">
+    <VueFlow
+      v-model="elements"
+      :default-viewport="{ zoom: 1, x: 0, y: 0 }"
+      :nodes-draggable="false"
+      :nodes-connectable="false"
+      :elements-selectable="false"
+      :zoom-on-scroll="true"
+      :pan-on-drag="true"
+      class="flow-bg"
+      fit-view
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { VueFlow } from "@vue-flow/core";
+
+const elements = ref([]); // tablero vacío por ahora
+</script>
+
+<style scoped lang="scss">
+.flow-bg {
+  background-color: $board-color;
+  cursor: grab !important;
+  background-image: radial-gradient(#bbb 1px, transparent 1px);
+  background-size: 20px 20px;
+  height: 100vh;
+  width: 100vw;
+  padding: 0px !important;
+  margin: 0px !important
+}
+</style>
